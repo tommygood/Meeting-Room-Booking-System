d1ffb09f5adf8c48e584fa1eddbca32d06bba981
tommygood <tommy50508@gmail.com>
2024-09-06 19:31:30 +0800

    get db info from env if not undefined

--- src/api/utilities/config.js
+++ src/api/utilities/config.js
@@ -3,11 +3,11 @@
 module.exports = {
 	host : "http://localhost:3000",
 	db : {
-		host : "db.cymlab.ncu.im",
-		port : 3306,
-		name : "secretariat",
-		username : "secretariat",
-		password : "Sec@ncu"
+		host : process.env.host == undefined ? 'db.cymlab.ncu.im' : process.env.db_host,
+		port : process.env.port == undefined ? 3306 : process.env.db_port,
+		name : process.env.db_name == undefined ? "secretariat" : process.env.db_name,
+		username : process.env.db_username == undefined ? "secretariat" : process.env.db_username,
+		password : process.env.db_password == undefined ? "Sec@ncu" : process.env.db_password
 	},
 	telegram : {
 		bot_token : "6261510862:AAEHbnjlKupEgKMSn6umI2ARTLNxwUSxnCo",
