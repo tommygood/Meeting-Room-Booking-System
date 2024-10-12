9239dfb962175ed755f92b6d9a0238d885a09ead
tommygood <tommy50508@gmail.com>
2024-10-12 21:41:08 +0800

    fix typo.

--- src/MRBS-frontend/vitest.config.js
+++ src/MRBS-frontend/vitest.config.js
@@ -8,12 +8,8 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-<<<<<<< HEAD
       root: fileURLToPath(new URL('./', import.meta.url)),
       globalSetup: './vitest.global-setup.ts',
-=======
-      root: fileURLToPath(new URL('./', import.meta.url))
->>>>>>> 3c1e13b14ce5178a1bb9cca590dda1a60f3a5fb9
     }
   })
 )
